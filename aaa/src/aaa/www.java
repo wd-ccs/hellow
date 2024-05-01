207be537b231bbda66107bb8d27d231baa392e50
wdssc <3391078625@qq.com>
2024-05-01 22:29:33 +0800

    first commit

---
+++ aaa/src/aaa/www.java
@@ -0,0 +1,19 @@
+package aaa;
+
+import java.io.*;
+import java.util.*;
+
+public class www{
+	public static void main(String args[]) {
+		int a[]= {1,10};
+		int b=1;
+		a[b-1]=b=2;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		boolean flag=false;
+		if(flag=true) {
+			System.out.print(flag);
+			System.out.print(2);
+		}
+	}
+}
